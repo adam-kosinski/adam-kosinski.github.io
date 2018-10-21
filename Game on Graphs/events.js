5d2fb7a1044cd4aebd375935d6d03d5d813ade5a
Adam <myemail>
2018-10-21 17:31:16 -0400

    Added events.js to Game on Graphs

---
+++ Game on Graphs/events.js
@@ -0,0 +1,25 @@
+var graphInput = document.getElementById("graphInput");
+var firstPlayerInput = document.getElementById("firstPlayerInput");
+var calculateButton = document.getElementById("calculate");
+var output = document.getElementById("output");
+
+
+calculateButton.addEventListener("click",calculate);
+
+function calculate(){
+	//get inputs
+	var M_adj = JSON.parse(graphInput.value);
+	var player = firstPlayerInput.value;
+	
+	//log interpretation to console
+	console.log("Inputted Graph:");
+	console.log(M_adj);
+	console.log("First Player:", player);
+	
+	//run function to see if they can guarantee a win in this position, and output result
+	if(canPlayerForceWin(M_adj, player)) {
+		output.innerText = "Player "+player" can force a win from this position.";
+	} else {
+		output.innerText = "Player "+player" cannot force a win from this position.";
+	}
+}
