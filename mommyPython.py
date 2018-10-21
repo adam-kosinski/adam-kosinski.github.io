5d3ee672f411d83beb035231c3f252457c01d01f
Adam <myemail>
2018-10-21 17:09:52 -0400

    Transferred all my code into this repository

---
+++ mommyPython.py
@@ -0,0 +1,17 @@
+
+def myFunction():
+    print("hi")
+
+x = 5
+
+
+for i in range(11):
+    print(i)
+
+
+myList = [[1,2,3],[4,5,6],[7,8,9]]
+myList.append("hi")
+for i in myList:
+    print(i)
+
+
