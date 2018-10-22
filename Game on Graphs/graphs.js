511efe56f9dd721e765fcffc0588fba4ff12a136
Adam <myemail>
2018-10-22 09:05:12 -0400

    fixed graphs.js error

--- Game on Graphs/graphs.js
+++ Game on Graphs/graphs.js
@@ -105,7 +105,7 @@ function makeMove(M_adj, M_saved, player,smv=[],cmv=[]){ //M_adj is the adjacenc
 	M_adj = copyMatrix(M_adj);
 	M_saved = copyMatrix(M_saved);
 	
-	console.log("function received",M_adj,M_saved)
+	//console.log("function received",M_adj,M_saved)
 	
 	//check if the other player won, if so, this player can't guarantee a win, return false
 	if(checkForWin(M_adj, M_saved, (player==="S"? "C" : "S"))) {return false}
@@ -122,19 +122,19 @@ function makeMove(M_adj, M_saved, player,smv=[],cmv=[]){ //M_adj is the adjacenc
 				var M_saved_test = copyMatrix(M_saved);
 				var smv_test = copyMatrix(smv)
 				var cmv_test = copyMatrix(cmv)
-				console.log("M_saved test after copy",M_saved_test)
+				//console.log("M_saved test after copy",M_saved_test)
 				if(player === "S"){
 					M_saved_test[r][c]++;
 					M_saved_test[c][r]++;
 					smv_test.push([r,c])
-					console.log("smv_test after move",smv_test)
+					//console.log("smv_test after move",smv_test)
 				}
 				if(player === "C"){
 					M_adj_test[r][c]--;
 					M_adj_test[c][r]--;
 					cmv_test.push([r,c]);
 				}
-				console.log("S Move",r,c,M_adj_test,M_saved_test)
+				//console.log("S Move",r,c,M_adj_test,M_saved_test)
 				//check to see if that move won, if so, there was a way to guarantee a win from this position, return true
 				if(checkForWin(M_adj_test, M_saved_test, player)){return true}
 				
@@ -146,10 +146,10 @@ function makeMove(M_adj, M_saved, player,smv=[],cmv=[]){ //M_adj is the adjacenc
 				
 				//iterate through all possible moves for the opponent
 				for(var rr=0; rr<M_adj.length; rr++){ //iterate through rows 'rr' to avoid naming conflict with the other for loop
-					console.log("rr",rr);
+					//console.log("rr",rr);
 					for(cc=0; cc<M_adj[0].length; cc++){ //iterate through cols 'cc' to avoid naming conflict with the other for loop
-						console.log("rr",rr,"cc",cc)
-						console.log(M_adj_test[rr][cc] , M_saved_test[rr][cc])
+						//console.log("rr",rr,"cc",cc)
+						//console.log(M_adj_test[rr][cc] , M_saved_test[rr][cc])
 						
 						//test if can move here
 						if((M_adj_test[rr][cc] > 0) && (M_saved_test[rr][cc] < M_adj_test[rr][cc])) { //if there are edges, and not all the edges are in M_saved_test
@@ -170,7 +170,8 @@ function makeMove(M_adj, M_saved, player,smv=[],cmv=[]){ //M_adj is the adjacenc
 								M_saved_2ndTest[cc][rr]++;
 								cmv_2ndTest.push([rr,cc])
 							}
-							console.log("C move",rr,cc,M_adj_2ndTest,M_saved_2ndTest)
+							//console.log("smv_2ndTest",smv_2ndTest,"cmv_2ndTest",cmv_2ndTest)
+							//console.log("C move",rr,cc,M_adj_2ndTest,M_saved_2ndTest)
 							
 							//see if the player can't guarantee a win in this position
 							if(!makeMove(M_adj_2ndTest, M_saved_2ndTest, player,smv_2ndTest,cmv_2ndTest)){
@@ -180,15 +181,13 @@ function makeMove(M_adj, M_saved, player,smv=[],cmv=[]){ //M_adj is the adjacenc
 					}
 				}
 				
+				//if for all opponent moves given this certain player move, the position is a guaranteed win for the player, player can guarantee win
 				if(canWinAllBranchingPositions){return true}
-				
-				//finished looking at all opponent moves, for all of which there was a way for this player to guarantee a win; return true
-				//(if there had been such an opponent move, the function would've returned false already and stopped, and we wouldn't reach this point)
-				return true;
 				
 			} //end check if player could make a move in the row col location
 		} //close player move col iteration
 	} //close player move row iteration
 	
+	//if looked through all of first player moves, and there isn't one that will guarantee a win no matter what the opponent does, can't guarantee win; return false
 	return false;
 } //close recursive function definition
