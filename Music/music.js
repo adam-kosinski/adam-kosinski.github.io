5b5cbe7bcc51f2ab619495384c5e0776d4a5b2e7
Adam <myemail>
2019-10-24 00:18:31 -0400

    Beautified Music webpage

--- Music/music.js
+++ Music/music.js
@@ -5,15 +5,15 @@ for(let i=0; i<audios.length; i++){
 	audios[i].innerText = "Your browser doesn't appear to support web audio. Some browsers that do: the most recent versions of Chrome and Microsoft Edge";
 }
 
-//background image
-let html = document.getElementsByTagName("html")[0];
-let backImg = document.getElementById("background");
-backImg.style.width = html.clientWidth + "px";
 
-//parallax scrolling
+
+
+
+
+/*/parallax scrolling
 window.addEventListener("scroll",function(){
 	window.requestAnimationFrame(function(){
 		let scroll_y = window.pageYOffset;
 		backImg.style.top = (- 0.5 * scroll_y) + "px";
 	});
-});
+});*/
