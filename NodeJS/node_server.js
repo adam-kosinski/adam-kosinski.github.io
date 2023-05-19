b13e8d46eef7e3c814fc0b852c1557f418c9901c
Adam Kosinski <amk119@duke.edu>
2023-05-19 01:10:27 -0400

    fixed index.html bug in node_server

--- NodeJS/node_server.js
+++ NodeJS/node_server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const { dir } = require("console");
 const app = express();
 
 if(process.argv.length < 3){
@@ -9,12 +10,14 @@ if(process.argv.length < 3){
 
 const html_path = process.argv[2];
 const directory = path.dirname(html_path);
+console.log(html_path, directory)
+
+//need to call app.get() first, since if we use express.static first it will default to index.html always
+app.get("/", function(req, res){
+    res.sendFile(path.resolve(html_path)); //convert path to absolute
+});
 
 app.use(express.static(directory));
-
-app.get("/", function(req, res){
-    res.sendFile(html_path);
-});
 
 const port = process.env.PORT || 8000;
 app.listen(8000, function(){
