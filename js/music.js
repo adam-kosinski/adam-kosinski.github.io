61d7ecf342074ed8098c21ee1f2857c148953c00
Adam Kosinski <amk119@duke.edu>
2023-12-30 23:04:00 -0500

    Music visualization implemented

--- js/music.js
+++ js/music.js
@@ -17,6 +17,13 @@ window.addEventListener("load", updateVisibleTracks);
 
 
 
+function updateVisualizationCanvasWidths(){
+    document.querySelectorAll(".visualization_canvas").forEach(canvas => {
+        canvas.width = canvas.getBoundingClientRect().width;
+    });
+}
+window.addEventListener("resize", updateVisualizationCanvasWidths);
+
 
 
 
@@ -44,13 +51,17 @@ document.querySelectorAll(".audio_player").forEach(player => {
     player.innerHTML = `
     <button class="play_pause"><div class="play_pause_icon"></div></button>
     <div class="time_and_seek">
-        <p class="time_display"><span class="current_time">0:00</span> / <span class="duration">0:00</span></p>
+        <div class="time_and_seek_top_row">
+            <p class="time_display"><span class="current_time">0:00</span> / <span class="duration">0:00</span></p>
+            <canvas height=20 class="visualization_canvas"></canvas>
+        </div>
         <div class="seek_bar_event_target">
             <div class="seek_bar">
                 <div class="seek_handle"></div>
             </div>
         </div>
-    </div>`;
+    </div>
+    `;
 
     const audio_element = new Audio();
     audio_element.preload = "metadata";
@@ -58,15 +69,88 @@ document.querySelectorAll(".audio_player").forEach(player => {
     player.append(audio_element); // easier to implement pause all audio function
 
     let context; // don't initialize until after user interaction, to avoid warning
+    let analyser;
+    let frequency_data;
+    let hz_per_freq_index; // hz interval that each index increase in frequency_data represents
+
+    let exp_coef; // exp_coef * exp_base ^ x_norm (where x_norm belongs to [0,1]) maps to the frequency plotted at that x
+    let exp_base;
+    // we use x_norm instead of x, because the canvas width might change and I don't want to refit the exponential function
+
+    const visualization_canvas = player.querySelector(".visualization_canvas");
+    // visualization config
+    const low_hz_bound = 5; // x = 0 will map to this hz
+    const high_hz_bound = 2000; // x = canvas width will map to this hz
+    const baseline_px = 2;
+    const top_clearance_px = 1; // so the stroke doesn't clip the canvas top
+
+    const initAudioContext = function () {
+        context = new AudioContext();
+
+        analyser = context.createAnalyser();
+        analyser.fftSize = 8192;
+        frequency_data = new Uint8Array(analyser.frequencyBinCount);
+        hz_per_freq_index = context.sampleRate / analyser.fftSize;
+
+        // calculate exp_coef and exp_base, we want:
+        // exp_coef * (exp_base ^ 0) = low_hz_bound
+        // exp_coef * (exp_base ^ 1) = high_hz_bound
+        exp_coef = low_hz_bound;
+        exp_base = high_hz_bound / exp_coef;
+
+        const track = context.createMediaElementSource(audio_element);
+        track.connect(analyser);
+        analyser.connect(context.destination);
+    }
+
+    const visualize = function () {
+        requestAnimationFrame(visualize);
+        if (!frequency_data) return;
+
+        analyser.getByteFrequencyData(frequency_data);
+
+        // draw a frame
+        const ctx = visualization_canvas.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.beginPath();
+
+        let x = 0;
+        let prev_freq_index;
+
+        for (; x < ctx.canvas.width; x++) {
+
+            const x_norm = x / ctx.canvas.width; // using x_norm instead of x because canvas width isn't guaranteed to stay the same
+            const freq_index = Math.floor(exp_coef * (exp_base ** x_norm));
+
+            if (freq_index < 0 || freq_index >= frequency_data.length) break; // just in case
+            if (prev_freq_index && freq_index == prev_freq_index) continue; //smoothing, don't add a new point if we don't have new info
+            prev_freq_index = freq_index;
+            
+            const amplitude_fraction = frequency_data[freq_index] / 255;
+            // raise amplitude (0-1) to a power to emphasize strong frequencies, and deemphasize quieter ones
+            const y = (ctx.canvas.height - baseline_px) - amplitude_fraction**1.5 * (ctx.canvas.height - baseline_px - top_clearance_px);
+            ctx.lineTo(x, y);
+        }
+
+        ctx.lineTo(x, ctx.canvas.height - baseline_px);
+        ctx.strokeStyle = "white";
+        ctx.stroke();
+
+        //do baseline bit
+        ctx.lineTo(x, ctx.canvas.height);
+        ctx.lineTo(0, ctx.canvas.height);
+
+        ctx.closePath();
+        ctx.fillStyle = "#fff3";
+        ctx.fill();
+    }
+    visualize();
 
     // play button triggers audio
     const play_button = player.querySelector(".play_pause");
     play_button.addEventListener("click", e => {
-        if (!context) {
-            context = new AudioContext();
-            const track = context.createMediaElementSource(audio_element);
-            track.connect(context.destination);
-        }
+        if (!context) initAudioContext();
+
         if (player.classList.contains("playing")) {
             audio_element.pause();
         }
@@ -100,7 +184,7 @@ document.querySelectorAll(".audio_player").forEach(player => {
         if (player.classList.contains("seeking")) return;
         setCurrentTimeDisplay(audio_element.currentTime);
     });
-    window.addEventListener("resize", e => {setCurrentTimeDisplay(audio_element.currentTime);});
+    window.addEventListener("resize", e => { setCurrentTimeDisplay(audio_element.currentTime); });
 
     // seek
     let seek_time_target = 0; // store where we want to seek until we're done seeking, only then update audio's current time
@@ -125,6 +209,8 @@ document.querySelectorAll(".audio_player").forEach(player => {
             audio_element.currentTime = seek_time_target;
             player.classList.remove("seeking");
             document.body.style.userSelect = "initial";
-        } 
+        }
     });
 });
+
+updateVisualizationCanvasWidths();
